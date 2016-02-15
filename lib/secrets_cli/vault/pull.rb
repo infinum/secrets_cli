f701ee044276d8ed87e50f5482529da5503f10bf
Stjepan Hadjic <d4be4st@gmail.com>
2016-02-15 13:13:25 +0100

    Add directory to pull

--- lib/secrets_cli/vault/pull.rb
+++ lib/secrets_cli/vault/pull.rb
@@ -1,17 +1,18 @@
 module SecretsCli
   module Vault
     class Pull < SecretsCli::Vault::Read
-      attr_reader :secrets_file
+      attr_reader :secrets_file, :secrets_dir
 
       def initialize(options)
         super
         @secrets_file = options.secrets_file || config.secrets_file
+        @secrets_dir = options.secrets_dir || '.'
       end
 
       def call
         secrets = super.first
         print_verbose("Writing to #{secrets_file}")
-        File.open(secrets_file, 'w') { |f| f.write(secrets) }
+        File.open(File.join(secrets_dir, secrets_file), 'w') { |f| f.write(secrets) }
       end
     end
   end
