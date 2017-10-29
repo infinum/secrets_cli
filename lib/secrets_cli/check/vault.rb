c8a0feb77a2a3960dd39cfa6979608728a81a5d5
štef <d4be4st@gmail.com>
2017-10-29 10:40:04 +0100

    add approle, resolves #4

--- lib/secrets_cli/check/vault.rb
+++ lib/secrets_cli/check/vault.rb
@@ -12,9 +12,12 @@ module SecretsCli
       def call
         error! 'Missing VAULT_ADDR env' if ENV['VAULT_ADDR'].nil?
         error! 'Missing VAULT_AUTH_METHOD env' if missing_auth_method?
-        if auth_method == 'app_id'
+        case auth_method
+        when 'app_id'
           error! 'Missing VAULT_AUTH_APP_ID' if missing_auth_app_id?
           error! 'Missing VAULT_AUTH_USER_ID' if missing_auth_user_id?
+        when 'approle'
+          error! 'Missing VAULT_AUTH_ROLE_ID' if missing_auth_role_id?
         else
           error! 'Missing VAULT_AUTH_TOKEN env' if missing_auth_token?
         end
@@ -36,6 +39,10 @@ module SecretsCli
 
       def missing_auth_user_id?
         options.auth_user_id.nil? && ENV['VAULT_AUTH_USER_ID'].nil?
+      end
+
+      def missing_auth_role_id?
+        options.auth_role_id.nil? && ENV['VAULT_AUTH_ROLE_ID'].nil?
       end
 
       def auth_method
--- lib/secrets_cli/vault/auth.rb
+++ lib/secrets_cli/vault/auth.rb
@@ -6,15 +6,17 @@ module SecretsCli
       def initialize(options)
         super
         SecretsCli::Check::Vault.new(options).call
-        @auth_method = ENV['VAULT_AUTH_METHOD']
-        @auth_token = ENV['VAULT_AUTH_TOKEN']
-        @auth_app_id = ENV['VAULT_AUTH_APP_ID']
-        @auth_user_id = ENV['VAULT_AUTH_USER_ID']
+        @auth_method    = ENV['VAULT_AUTH_METHOD']
+        @auth_token     = ENV['VAULT_AUTH_TOKEN']
+        @auth_app_id    = ENV['VAULT_AUTH_APP_ID']
+        @auth_user_id   = ENV['VAULT_AUTH_USER_ID']
+        @auth_role_id   = ENV['VAULT_AUTH_ROLE_ID']
+        @auth_secret_id = ENV['VAULT_AUTH_SECRET_ID']
       end
 
       private
 
-      attr_reader :auth_token, :auth_method, :auth_app_id, :auth_user_id
+      attr_reader :auth_token, :auth_method, :auth_app_id, :auth_user_id, :auth_role_id, :auth_secret_id
 
       def command
         case auth_method
@@ -24,6 +26,8 @@ module SecretsCli
           ::Vault.auth.token(auth_token).auth.policies
         when 'app_id'
           ::Vault.auth.app_id(auth_app_id, auth_user_id).auth.policies
+        when 'approle'
+          ::Vault.auth.approle(auth_role_id, auth_secret_id).auth.policies
         else
           error! "Unknown auth method #{auth_method}"
         end
