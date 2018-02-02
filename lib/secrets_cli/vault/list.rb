6e74975b0355842bd192e902592fc591d3d9a171
Stjepan Hadjic <d4be4st@gmail.com>
2018-02-02 12:02:58 +0100

    add list

---
+++ lib/secrets_cli/vault/list.rb
@@ -0,0 +1,20 @@
+module SecretsCli
+  module Vault
+    class List < SecretsCli::Vault::Base
+      def initialize(options)
+        super
+        options.default(verbose: !options.ci_mode)
+        SecretsCli::Check::Secrets.new(:read, options).call
+        @secrets_storage_key = options.secrets_storage_key || config.secrets_storage_key
+      end
+
+      private
+
+      attr_reader :secrets_storage_key
+
+      def command
+        ::Vault.logical.list(secrets_storage_key).join("\n")
+      end
+    end
+  end
+end
